2e7a3b050754fefe8c8d6f17dbaf901e89a5f60a
pr000000f <yuchensoso@163.com>
2025-12-10 17:12:05 +0800

    Sync OpenSSL: Remove some unnecessary function pointers from OSSL_RECORD_METHOD
    From OpenSSL commit 81c9ebd9099e7aac92a8c855a9ae1a30bad1d9cc
    
    We had some temporary function pointers in OSSL_RECORD_METHOD which were
    only necessary during the process of refactoring the read record layer.
    These are no longer required so can be removed.

--- include/openssl/core_names.h
+++ include/openssl/core_names.h
@@ -565,6 +565,7 @@ extern "C" {
 #define OSSL_LIBSSL_RECORD_LAYER_PARAM_OPTIONS        "options"
 #define OSSL_LIBSSL_RECORD_LAYER_PARAM_MODE           "mode"
 #define OSSL_LIBSSL_RECORD_LAYER_PARAM_READ_AHEAD     "read_ahead"
+#define OSSL_LIBSSL_RECORD_LAYER_READ_BUFFER_LEN      "read_buffer_len"
 #define OSSL_LIBSSL_RECORD_LAYER_PARAM_USE_ETM        "use_etm"
 #define OSSL_LIBSSL_RECORD_LAYER_PARAM_MAX_FRAG_LEN   "max_frag_len"
 #define OSSL_LIBSSL_RECORD_LAYER_PARAM_MAX_EARLY_DATA "max_early_data"
