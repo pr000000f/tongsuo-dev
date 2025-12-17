e3c224d75893ab3c6123a9a693eaa756ef2cd616
pr000000f <yuchensoso@163.com>
2025-12-17 12:39:59 +0800

    Sync OpenSSL:  Fix URI handling in SSL_CERT_DIR/introduce  SSL_CERT_URI env
    From OpenSSL commit 021859bf810a3614758c2f4871b9cd7202fac9b2

--- include/internal/common.h
+++ include/internal/common.h
@@ -13,8 +13,7 @@
 
 # include <stdlib.h>
 # include <string.h>
-# include "e_os.h" /* To get strncasecmp() on Windows */
-
+# include "internal/e_os.h" /* To get strncasecmp() on Windows */
 # include "internal/nelem.h"
 
 #ifdef NDEBUG
@@ -71,6 +70,9 @@ __owur static ossl_inline int ossl_assert_int(int expr, const char *exprstr,
 #  define SMTC_AUTH_KEK          OPENSSLDIR "/auth_kek.pem"
 # endif
 
+#define X509_CERT_URI            ""
+
+# define X509_CERT_URI_EVP        "SSL_CERT_URI"
 # define X509_CERT_DIR_EVP        "SSL_CERT_DIR"
 # define X509_CERT_FILE_EVP       "SSL_CERT_FILE"
 # define CTLOG_FILE_EVP           "CTLOG_FILE"
@@ -98,6 +100,17 @@ static ossl_inline int ossl_is_absolute_path(const char *path)
         return 1;
 # endif
     return path[0] == '/';
+}
+
+static ossl_inline int ossl_is_uri(const char *s)
+{
+    const char *x;
+    for (x=s; ossl_isalnum(*x); ++x);
+#ifdef _WIN32
+    if (x-s <= 1)
+        return 0;
+#endif
+    return x > s && HAS_PREFIX(x, "://");
 }
 
 #endif
