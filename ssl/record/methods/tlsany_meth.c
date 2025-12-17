58bc7da16bac446d68f52879dea7b398be39a0f3
pr000000f <yuchensoso@163.com>
2025-12-17 19:06:31 +0800

    Sync OpenSSL: Defer write buffer and WPACKET allocation/initialisation to protocol code
    From OpenSSL commit 91fe8ff02a323eddb0404f975d5c9a03c024593b

--- ssl/record/methods/tlsany_meth.c
+++ ssl/record/methods/tlsany_meth.c
@@ -150,7 +150,9 @@ struct record_functions_st tls_any_funcs = {
     tls_validate_record_header,
     tls_default_post_process_record,
     tls_get_max_records_default,
-    tls_write_records_default
+    tls_write_records_default,
+    tls_allocate_write_buffers_default,
+    tls_initialise_write_packets_default
 };
 
 static int dtls_any_set_protocol_version(OSSL_RECORD_LAYER *rl, int vers)
@@ -169,6 +171,8 @@ struct record_functions_st dtls_any_funcs = {
     dtls_any_set_protocol_version,
     tls_default_read_n,
     dtls_get_more_records,
+    NULL,
+    NULL,
     NULL,
     NULL,
     NULL,
