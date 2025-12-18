28ad2e1c6db0a5abc460bf9c2d3b714b2a1cdb4e
pr000000f <yuchensoso@163.com>
2025-12-18 12:10:25 +0800

    Sync OpenSSL: Remove the old buffer management code
    From OpenSSL commit e158ada6a74e5903354fdd5a6f56a32bbbba69fd

--- ssl/ssl_local.h
+++ ssl/ssl_local.h
@@ -2568,7 +2568,6 @@ const SSL_METHOD *func_name(void)  \
 
 struct openssl_ssl_test_functions {
     int (*p_ssl_init_wbio_buffer) (SSL_CONNECTION *s);
-    int (*p_ssl3_setup_buffers) (SSL_CONNECTION *s);
 };
 
 const char *ssl_protocol_to_string(int version);
@@ -3098,7 +3097,6 @@ void ssl_session_calculate_timeout(SSL_SESSION *ss);
 # else /* OPENSSL_UNIT_TEST */
 
 #  define ssl_init_wbio_buffer SSL_test_functions()->p_ssl_init_wbio_buffer
-#  define ssl3_setup_buffers SSL_test_functions()->p_ssl3_setup_buffers
 
 # endif
 
